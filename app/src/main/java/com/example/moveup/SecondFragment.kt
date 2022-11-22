e494e37820c586a57af68f2db1fa16e596e713ef
Annemarie Kayser <annemarie.kayser@stud.hshl.de>
2022-11-22 12:35:07 +0100

    Darstellen eines graphs in fragment_second

--- app/src/main/java/com/example/moveup/SecondFragment.kt
+++ app/src/main/java/com/example/moveup/SecondFragment.kt
@@ -8,6 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.moveup.databinding.FragmentSecondBinding
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartStackingType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -35,6 +39,30 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Column)
+            .title(getString(R.string.chart_title))
+            //.subtitle("subtitle")
+            .backgroundColor("#FFFFFFFF")
+            .dataLabelsEnabled(false)
+            .stacking(AAChartStackingType.Percent)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Tokyo")
+                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
+                AASeriesElement()
+                    .name("NewYork")
+                    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
+                AASeriesElement()
+                    .name("London")
+                    .data(arrayOf(0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0)),
+                AASeriesElement()
+                    .name("Berlin")
+                    .data(arrayOf(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8))
+            )
+            )
+
+        binding.chartView.aa_drawChartWithChartModel(aaChartModel)
     }
 
     override fun onDestroyView() {
